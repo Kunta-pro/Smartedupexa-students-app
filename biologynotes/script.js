cba0bca0d24f98256407a4f320a99fd5595b0992
Kunta-pro <smartedupexa@gmail.com>
2024-12-28 16:03:52 +0300

    Update script.js

--- biologynotes/script.js
+++ biologynotes/script.js
@@ -1,2 +1,4 @@
 // Placeholder for any interactivity or additional scripts if needed
 // For now, the links will open the respective pages when the tiles are clicked
+// The script is for the sliding functionality in the banner
+// This will handle the auto-scrolling of book images in the banner section
