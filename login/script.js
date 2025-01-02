8e55d93cca71f16f1f262ba136673cf19e1f0803
Kunta-pro <smartedupexa@gmail.com>
2025-01-02 09:52:07 +0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,7 +1,7 @@
 // Array of predefined usernames and passwords (you can customize these later)
 const validUsers = [
     { username: "user1", password: "password1" },
-    { username: "user2", password: "password2" },
+    { username: "Saudah", password: "saudahnabs2" },
     { username: "admin", password: "admin123" },
 ];
 
